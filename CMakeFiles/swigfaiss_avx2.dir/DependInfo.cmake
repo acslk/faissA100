6f416700994c969c7c686a70505656faa44dedda
Dave Li <yeli1@microsoft.com>
2022-02-22 18:22:40 +0000

    init

---
+++ CMakeFiles/swigfaiss_avx2.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/webdata-nfs/yeli1/dep/faiss/build/faiss/python/CMakeFiles/swigfaiss_avx2.dir/swigfaiss_avx2PYTHON_wrap.cxx" "/webdata-nfs/yeli1/dep/faiss/build/faiss/python/CMakeFiles/swigfaiss_avx2.dir/CMakeFiles/swigfaiss_avx2.dir/swigfaiss_avx2PYTHON_wrap.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "swigfaiss_avx2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../faiss/python/../.."
+  "../"
+  "/usr/local/cuda/include"
+  "/opt/conda/include/python3.8"
+  "/opt/conda/lib/python3.8/site-packages/numpy/core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/webdata-nfs/yeli1/dep/faiss/build/faiss/CMakeFiles/faiss_avx2.dir/DependInfo.cmake"
+  "/webdata-nfs/yeli1/dep/faiss/build/faiss/python/CMakeFiles/faiss_python_callbacks.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/swigfaiss_avx2_swig_compilation.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/webdata-nfs/yeli1/dep/faiss/faiss/python/swigfaiss_avx2.swig" "/webdata-nfs/yeli1/dep/faiss/build/faiss/python/CMakeFiles/swigfaiss_avx2.dir/swigfaiss_avx2PYTHON.stamp"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
